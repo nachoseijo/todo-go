e84382e54b7543ccc4cdd416760321161b0c9bd2
Nacho <nacho.seijo@gmail>
2020-05-02 23:26:26 +0200

    updated mod and sum changin project name

--- main.go
+++ main.go
@@ -4,14 +4,13 @@ import (
 	"log"
 
 	"github.com/gin-gonic/gin"
-	"github.com/nachoseijo/todo/routes"
 )
 
 func main() {
 	//init a router
 	router := gin.Default()
 
-	Routes.routes(router)
+	//Routes.routes(router)
 
 	log.Fatal(router.Run(":8090"))
 }
