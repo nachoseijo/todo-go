4759e97853c294dfbd19c2578569b0cb8ca806e2
Nacho <nacho.seijo@gmail>
2020-05-06 01:10:46 +0200

    enabled cors and added support for enviroment variables

--- main.go
+++ main.go
@@ -2,28 +2,30 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"todo-go/database"
 	"todo-go/routes"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
-//Needs to be modified in order to take the value from env vars
-const databaseURI = "mongodb://database:27017"
-const databaseName = "todo-go"
-const collection = "todos"
-
 func main() {
+	apiPort := os.Getenv("API_PORT")
+	databaseURI := os.Getenv("DATABASE_URI")
+	databaseName := os.Getenv("DATABASE_NAME")
+	collection := os.Getenv("COLLECTION")
 
 	//Connect to Database
 	database.Connect(collection, databaseName, databaseURI)
 
 	//Inits a router
 	router := gin.Default()
-
+	//Enables cors for all origins
+	router.Use(cors.Default())
 	//Router Handlers
 	routes.Routes(router)
 
-	log.Fatal(router.Run(":80"))
+	log.Fatal(router.Run(":" + apiPort))
 }
